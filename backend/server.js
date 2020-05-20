64b528ee5d1026544bcc39c651ab2b02f782d759
josedaniel.vasquez <josedaniel.vasquez@globant.com>
2020-05-20 16:28:04 -0500

    Server - add json data, create get for /plates

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const data = require('./src/assets/data.json');
 
 const app = express();
 
 app.use(bodyParser.json());
+
+// initialize data (plates)
+const plates = data;
+
+
+// GET
+app.get('/plates', (req, res) => {
+    res.json(plates);
+});
 
 app.listen(4000, () => {
     console.log('Server initialized on: 4000');
