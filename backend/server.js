22ecd5710aa149f144641515df23c968e0c56f30
josedaniel.vasquez <josedaniel.vasquez@globant.com>
2020-05-20 16:59:45 -0500

    Server - add first middleware, existingPlate

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const data = require('./src/assets/data.json');
 
+// Middlewares
+const existingPlate = require('./src/middlewares/existing-plate');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -9,10 +12,16 @@ app.use(bodyParser.json());
 // initialize data (plates)
 const plates = data;
 
-
 // GET
 app.get('/plates', (req, res) => {
     res.json(plates);
+});
+
+// POST
+app.post('/plates', existingPlate.existingData, (req, res) => {
+    
+    plates.push(req.body);
+    res.json(req.body);
 });
 
 app.listen(4000, () => {
